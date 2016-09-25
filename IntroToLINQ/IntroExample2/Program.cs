15a7ab37e9a2271ee4b3d852b6badbae114ff391
riyad291 <ririyad.291@gmail.com>
2016-09-25 06:59:00 +0600

    upto LINQ method syntax

---
+++ IntroToLINQ/IntroExample2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> stringList = new List<string>()
+            {
+                 "C# Tutorials",
+                 "VB.NET Tutorials",
+                 "Learn C++",
+                 "MVC Tutorials" ,
+                 "Java"
+            };
+
+            // using query syntax
+            var result = from s in stringList
+                where s.Contains("Tutorials")
+                select s;
+
+            // using method syntax
+            // method syntax = .extension methods(lambada expressions)
+            var result2 = stringList.Where(s => s.Contains("Tutorials"));
+            foreach (var str in result2)
+            {
+                Console.WriteLine(str);
+            }
+
+        }
+    }
+}
--- IntroToLINQ/IntroToLINQ/Program.cs
+++ IntroToLINQ/IntroToLINQ/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace IntroToLINQ
 {
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Student[] studentArray =
+            {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },                
+            };
+
+            // using lambda expression
+            Student[] teenAgeStudents = studentArray.Where(s => s.Age > 12 && s.Age < 20).ToArray();
+
+            // using query expression
+           var teenStudents2 = from teenage in studentArray
+                where teenage.Age > 12 && teenage.Age < 20
+                select teenage;
+
+           foreach (var std in teenStudents2)
+                Console.WriteLine(std.StudentName);
         }
     }
 }
